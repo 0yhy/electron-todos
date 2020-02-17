0c60c8d04094601046250552ffa26022ce9c9eca
LucyShawYang <lucyshawyang@outlook.com>
2020-02-17 20:59:07 +0800

    edit preload db

--- renderer.js
+++
@@ -1,5 +0,0 @@
-global.electron = require("electron");
-global.low = require("lowdb");
-global.FileSync = require("lowdb/adapters/FileSync");
-const apdater = new FileSync("userinfo.json");
-global.db = low(apdater);
